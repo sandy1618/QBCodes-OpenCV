779a28d6752fa19a3df62d4c8e64b3ad5847ded1
SpawnTree <shekharhiran@hotmail.com>
2019-05-26 10:23:25 +0530

    Image_generation_store_mapping

---
+++ gcc-application/opencv-bcodes/opencv_dev/opencv_dev.cpp
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <bits/stdc++.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+/* Maximum Window Area */
+#define MAX_X 125
+#define MAX_Y 125
+
+/* Min Max radius of BCode */
+#define min_radius 45 
+#define max_radius 55
+#define finite_module_size 5
+#define thickness 1
+
+/* Random numbers range */
+#define MIN_RANGE 1
+#define MAX_RANGE 99999999
+
+using namespace cv;
+using namespace std;
+
+int main (int argc, char* argv[], char* envp[])
+{
+	s
+	
+	return 0;
+}
